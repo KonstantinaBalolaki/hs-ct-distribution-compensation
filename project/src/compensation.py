390a14d3bfc59ca034e32c84912561deb4a59082
KonstantinaBalolaki <92733132+KonstantinaBalolaki@users.noreply.github.com>
2026-05-12 12:52:50 +0200

    Update compensation.py

--- project/src/compensation.py
+++ project/src/compensation.py
@@ -1,1 +1,16 @@
+import numpy as np
 
+def quantile_compensation(hs_dev, ct_dev):
+
+    q = np.linspace(0,1,1000)
+
+    hs_q = np.quantile(hs_dev, q)
+    ct_q = np.quantile(ct_dev, q)
+
+    comp = np.interp(
+        hs_dev,
+        hs_q,
+        ct_q
+    )
+
+    return comp
