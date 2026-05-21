886768424ae0c978f7e95ed13e649fe4e52878f3
KonstantinaBalolaki <92733132+KonstantinaBalolaki@users.noreply.github.com>
2026-05-21 10:29:15 +0200

    Enhance quantile_compensation with docstring and n_quantiles
    Added docstring and parameter for number of quantiles.

--- project/src/quantile_mapping.py
+++ project/src/quantile_mapping.py
@@ -1,16 +1,27 @@
 import numpy as np
 
-def quantile_compensation(hs_dev, ct_dev):
 
-    q = np.linspace(0,1,1000)
+def quantile_compensation(hs_dev, ct_dev, n_quantiles=1000):
+    """
+    Apply quantile mapping to transform HandySCAN deviations
+    into CT-reference-equivalent deviations.
+    """
+
+    hs_dev = np.asarray(hs_dev)
+    ct_dev = np.asarray(ct_dev)
+
+    hs_dev = hs_dev[~np.isnan(hs_dev)]
+    ct_dev = ct_dev[~np.isnan(ct_dev)]
+
+    q = np.linspace(0, 1, n_quantiles)
 
     hs_q = np.quantile(hs_dev, q)
     ct_q = np.quantile(ct_dev, q)
 
-    comp = np.interp(
+    compensated_dev = np.interp(
         hs_dev,
         hs_q,
         ct_q
     )
 
-    return comp
+    return compensated_dev
